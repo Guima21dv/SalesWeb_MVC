c036c4c3753f9b82468c5bb32bbcebe35a62caad
Guilherme <guima_dev@hotmail.com>
2020-05-06 15:04:40 -0300

    Implementado tela Index para Sellers

--- SalesWeb/Controllers/SellersController.cs
+++ SalesWeb/Controllers/SellersController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWeb.Services;
 
 namespace SalesWeb.Controllers
 {
     public class SellersController : Controller
     {
+
+        private readonly SellerService _service;
+
+        public SellersController(SellerService service)
+        {
+            _service = service;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var sellers = _service.FindAll();
+            return View(sellers);
         }
     }
 }
